cef04d5ab475d41be3bb8617848bbad19575f250
tibbi <tibor@kaputa.sk>
2016-10-25 20:15:57 +0200

    add some file type helper functions to FileDirItem

--- library/src/main/kotlin/com/simplemobiletools/filepicker/models/FileDirItem.kt
+++ library/src/main/kotlin/com/simplemobiletools/filepicker/models/FileDirItem.kt
@@ -1,5 +1,8 @@
 package com.simplemobiletools.filepicker.models
 
+import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
+
 class FileDirItem(val path: String, val name: String, val isDirectory: Boolean, val children: Int, val size: Long) :
         Comparable<FileDirItem> {
 
@@ -15,5 +18,26 @@ class FileDirItem(val path: String, val name: String, val isDirectory: Boolean, 
 
     override fun toString(): String {
         return "FileDirItem{name=$name, isDirectory=$isDirectory, path=$path, children=$children, size=$size}"
+    }
+
+    fun isGif() = name.toLowerCase().endsWith(".gif")
+    fun isVideo() = getMimeType().startsWith("video")
+
+    fun isImage(): Boolean {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(path, options)
+        return options.outWidth != -1 && options.outHeight != -1
+    }
+
+    fun getMimeType(): String {
+        try {
+            val retriever = MediaMetadataRetriever()
+            retriever.setDataSource(path)
+            return retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_MIMETYPE)
+        } catch (ignored: Exception) {
+
+        }
+        return ""
     }
 }
