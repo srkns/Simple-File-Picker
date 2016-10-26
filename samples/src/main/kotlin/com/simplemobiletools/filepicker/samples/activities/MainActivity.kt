8cc4670226c67a35b06bc5c18f4364a2a81dd5f3
tibbi <tibor@kaputa.sk>
2016-10-26 20:11:14 +0200

    handle storage permission requesting

--- samples/src/main/kotlin/com/simplemobiletools/filepicker/samples/activities/MainActivity.kt
+++ samples/src/main/kotlin/com/simplemobiletools/filepicker/samples/activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.simplemobiletools.filepicker.samples.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import com.simplemobiletools.filepicker.dialogs.FilePickerDialog
 import com.simplemobiletools.filepicker.dialogs.FilePickerDialog.FilePickerResult.DISMISS
@@ -11,6 +15,11 @@ import com.simplemobiletools.filepicker.samples.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val STORAGE_PERMISSION = 1
+    val PICK_FILE = 1
+    val PICK_FOLDER = 2
+
+    var action = PICK_FILE
     lateinit var home: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +32,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun pickFile() {
+        if (!hasStoragePermission()) {
+            action = PICK_FILE
+            requestStoragePermission()
+            return
+        }
+
         FilePickerDialog(this, listener = object : FilePickerDialog.OnFilePickerListener {
             override fun onFail(error: FilePickerDialog.FilePickerResult) {
                 when (error) {
@@ -39,6 +54,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun pickFolder() {
+        if (!hasStoragePermission()) {
+            action = PICK_FOLDER
+            requestStoragePermission()
+            return
+        }
+
         FilePickerDialog(this, pickFile = false, listener = object : FilePickerDialog.OnFilePickerListener {
             override fun onFail(error: FilePickerDialog.FilePickerResult) {
                 when (error) {
@@ -52,5 +73,18 @@ class MainActivity : AppCompatActivity() {
                 picked_folder_path.text = path
             }
         })
+    }
+
+    private fun requestStoragePermission() = ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION)
+    private fun hasStoragePermission() = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION && grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            when (action) {
+                PICK_FILE -> pickFile()
+                else -> pickFolder()
+            }
+        }
     }
 }
