2eb917a5755c87d8355121715b5f5f015ab5a29d
tibbi <tibor@kaputa.sk>
2016-12-08 19:38:29 +0100

    no need to update the size at renaming or moving files

--- library/src/main/kotlin/com/simplemobiletools/filepicker/extensions/context.kt
+++ library/src/main/kotlin/com/simplemobiletools/filepicker/extensions/context.kt
@@ -143,7 +143,6 @@ fun Context.deleteFromMediaStore(file: File) = contentResolver.delete(getFileUri
 fun Context.updateInMediaStore(oldFile: File, newFile: File): Boolean {
     val values = ContentValues().apply {
         put(MediaStore.MediaColumns.DATA, newFile.absolutePath)
-        put(MediaStore.MediaColumns.SIZE, newFile.length())
         put(MediaStore.MediaColumns.DISPLAY_NAME, newFile.name)
         put(MediaStore.MediaColumns.TITLE, newFile.name)
     }
